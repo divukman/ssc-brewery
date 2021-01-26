99832cc00b5fbe8d44d7889d4407202fdd76c200
dimitar <dimitar.vukman@bluedropmedical.com>
2021-01-26 13:47:11 +0100

    Auth success listener

---
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationSuccessListener.java
@@ -0,0 +1,20 @@
+package guru.sfg.brewery.security.listeners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class AuthenticationSuccessListener {
+
+    @EventListener
+    public void listen(AuthenticationSuccessEvent event) {
+        log.info("Auth success...");
+        log.debug("**********************************************************");
+        log.debug("Auth success...");
+        log.debug("NAME: " + event.getAuthentication().getName());
+        log.debug("**********************************************************");
+    }
+}
